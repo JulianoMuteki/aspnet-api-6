7a782e13351efeeb8ce93fb510c48d6fd5a0db50
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2022-04-27 21:05:53 -0300

    Clear references

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyWebAPI.Models;
-using System;
 using Microsoft.AspNetCore.Authorization;
-using System.Linq;
 using MyWebAPI.Services;
 using MyWebAPI.Repositories;
 
